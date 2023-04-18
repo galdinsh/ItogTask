46282f49aafdda9dfcd212717e8bcbd83fcf5b74
Vladislav <galdinsh77@mail.ru>
2023-04-18 16:57:05 +0500

    Создал процедуру формирования нового массива

--- TaskFinal/Program.cs
+++ TaskFinal/Program.cs
@@ -1,2 +1,16 @@
-﻿string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
+﻿void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+
+string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
 string[] array2 = new string[array1.Length]; 
